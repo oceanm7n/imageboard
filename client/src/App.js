da9e29d4be36c8801f41e2c42d206466b2cce9d8
Alex <alex@10.0.0.4>
2019-05-21 21:05:32 +0300

    navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import AppNavbar from './components/Navbar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
-  return (
-    <div>
-      <h1>
-        app on port 3000
-      </h1>
-    </div>
-  );
+    return (
+        <div>
+            <AppNavbar />
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+class AppNavbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/">Imageboard</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/oceanm7n" target="_blank">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Boards
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    <NavLink href="/b" style={{"color":"black"}}>/b/</NavLink>
+                  </DropdownItem>
+                  <DropdownItem>
+                    <NavLink href="/int" style={{"color":"black"}}>/int/</NavLink>
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;
